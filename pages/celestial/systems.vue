0600010ef13236b9964e07b32e61044ef28973b4
Derrick Mehaffy <derrickmehaffy@gmail.com>
2020-08-22 22:54:11 -0700

    WIP Refactor
    Against Garud's single comp but easier to handle

--- pages/celestial/systems.vue
+++ pages/celestial/systems.vue
@@ -4,33 +4,52 @@
       Systems
       <v-spacer></v-spacer>
       <v-text-field
-        v-model="search"
+        v-model="options.search"
         append-icon="mdi-magnify"
-        label="Search"
+        label="Search System Name"
         single-line
         hide-details
       ></v-text-field>
     </v-card-title>
     <v-spacer></v-spacer>
     <v-card flat>
-      <reports-table
+      <v-data-table
         :headers="headers"
-        :records="systems"
-        :search="search"
-      ></reports-table>
+        :items="systems"
+        :options.sync="options"
+        :server-items-length="systemsCount"
+        :footer-props="{ 'items-per-page-options': [10, 15, 30, 50] }"
+        :items-per-page="15"
+        :loading="loading"
+      >
+        <template v-slot:[`item.edsmCoordLocked`]="{ item }">
+          <v-simple-checkbox v-model="item.edsmCoordLocked" disabled>
+          </v-simple-checkbox>
+        </template>
+        <template v-slot:[`item.primaryStar.isScoopable`]="{ item }">
+          <v-simple-checkbox v-model="item.primaryStar.isScoopable" disabled>
+          </v-simple-checkbox>
+        </template>
+      </v-data-table>
     </v-card>
   </v-card>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import ReportsTable from '../../components/ReportsTable'
+import axios from 'axios'
+import consola from 'consola'
+
+// Got this code from: https://next.vuetifyjs.com/en/components/data-tables/#server-side-paginate-and-sort
 
 export default {
-  components: { ReportsTable },
   data() {
     return {
-      search: '',
+      systemsCount: 0,
+      systems: [],
+      loading: true,
+      options: {
+        search: ''
+      },
       headers: [
         {
           text: 'Name',
@@ -41,21 +60,87 @@ export default {
         { text: 'Coord X', value: 'edsmCoordX' },
         { text: 'Coord Y', value: 'edsmCoordY' },
         { text: 'Coord Z', value: 'edsmCoordZ' },
-        { text: 'Locked', value: 'edsmCoordLocked' },
-        { text: 'Region', value: 'region.name' },
-        { text: 'Star Type', value: 'primaryStar.type' },
-        { text: 'Scoopable', value: 'primaryStar.isScoopable' }
+        { text: 'Coords Locked', value: 'edsmCoordLocked' },
+        { text: 'Region', sortable: false, value: 'region.name' },
+        { text: 'Star Type', sortable: false, value: 'primaryStar.type' },
+        {
+          text: 'Scoopable',
+          sortable: false,
+          value: 'primaryStar.isScoopable'
+        }
       ]
     }
   },
-  computed: {
-    ...mapState({
-      systems: state => state.celestial.systems
+  watch: {
+    options: {
+      async handler() {
+        const data = await this.getDataFromApi()
+        this.systems = data.items
+        this.systemsCount = data.total
+      },
+      deep: true
+    }
+  },
+  mounted() {
+    this.getDataFromApi().then(data => {
+      this.systems = data.items
+      this.systemsCount = data.total
     })
   },
-  created() {
-    this.$store.dispatch('celestial/fetchSystems')
-    this.loading = false
+  methods: {
+    async getDataFromApi() {
+      this.loading = true
+      const { sortBy, sortDesc, page, itemsPerPage, search } = this.options
+
+      const data = await this.getSystems(
+        page,
+        itemsPerPage,
+        sortBy,
+        sortDesc,
+        search
+      )
+
+      this.loading = false
+      return {
+        items: data.systems,
+        total: data.count
+      }
+    },
+    async getSystems(page, limit, sortBy, sortDesc, search) {
+      const host = 'https://api.canonn.tech/systems'
+      let url = `${host}?_limit=${limit}`
+      let start = 0
+
+      // Set pages
+      // Need to figure out how to construct the start from the page
+      if (page > 1) {
+        start = page * start
+      }
+
+      // Apply sorting
+      if (sortBy.length > 0) {
+        if (sortDesc[0] === true) {
+          url = url + `&_sort=${sortBy[0]}:desc`
+        } else {
+          url = url + `&_sort=${sortBy[0]}:asc`
+        }
+      }
+
+      // Apply Searching
+      // Need to fix search + paging, if user has paged then searches returns nothing
+      if (search) {
+        url = url + `&_q=${search}`
+      }
+
+      url = url + `&_start=${start}`
+      consola.log(url)
+      const response = await axios.get(url)
+
+      return {
+        systems: response.data,
+        count: parseInt(response.headers['content-range'])
+      }
+    }
   }
 }
 </script>
